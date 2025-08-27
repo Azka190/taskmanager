5409fbf6833e061947640e18b8775a22b48f5c61
Azka190 <azkarehman2001@gmail.com>
2025-08-27 16:20:26 +0500

    FIX: types

---
+++ types/aos.d.ts
@@ -0,0 +1,22 @@
+declare module "aos" {
+    export interface AosOptions {
+      offset?: number;
+      delay?: number;
+      duration?: number;
+      easing?: string;
+      once?: boolean;
+      mirror?: boolean;
+      anchorPlacement?: string;
+    }
+  
+    export function init(options?: AosOptions): void;
+    export function refresh(): void;
+  
+    const AOS: {
+      init(options?: AosOptions): void;
+      refresh(): void;
+    };
+  
+    export default AOS;
+  }
+  
